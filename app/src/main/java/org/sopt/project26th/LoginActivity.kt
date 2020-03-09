e1562a776c7ccb2033790e107de580010da8b375
CHJUNE-android <chjune0205@gmail.com>
2020-03-09 09:30:57 +0900

    1차 세미나 자료

---
+++ app/src/main/java/org/sopt/project26th/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.project26th
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/org/sopt/project26th/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.project26th
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
